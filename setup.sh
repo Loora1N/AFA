c64018d695ac9dcb0ae4c31f9475b0c58b70275c
Loora1N <15129303223@163.com>
2023-06-29 16:04:55 +0800

    delete python-psycopg2

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@
 
 sudo apt update
 #firmadyne
-sudo apt-get install -y busybox-static fakeroot git dmsetup kpartx netcat-openbsd nmap python-psycopg2 python3-psycopg2 snmp uml-utilities util-linux vlan
+sudo apt-get install -y busybox-static fakeroot git dmsetup kpartx netcat-openbsd nmap python3-psycopg2 snmp uml-utilities util-linux vlan
 #firmwalker
 npm i -g eslint
